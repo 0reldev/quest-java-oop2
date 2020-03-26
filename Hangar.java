1ead63602bcb6cf564a0f8bbe3f94ec8507ae1ff
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-03-26 17:11:17 +0100

    Exercice solved: creation of Boat.java, Car.Java, Hangar.java an Vehicle.java

---
+++ Hangar.java
@@ -0,0 +1,9 @@
+public class Hangar {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Opel", 144000);
+        Boat myBoat = new Boat("Amel", 2000);
+            System.out.println(myCar.doStuff());
+            System.out.println(myBoat.doStuff());
+    }
+}
