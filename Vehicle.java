82c8fc21a8721841e98ae575828c3cf7cbda82fe
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-09-15 10:24:25 +0200

    Code cleaning 2

--- Vehicle.java
+++ Vehicle.java
@@ -3,7 +3,8 @@ public abstract class Vehicle {
     private String brand;  
     private int kilometers;  
 
-    public Vehicle(String brand, int kilometers) {  
+    public Vehicle(String brand, int kilometers) {
+
         this.brand = brand;  
         this.kilometers = kilometers;  
     }  
